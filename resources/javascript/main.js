cd7813dd56e4878f41b285b393a9bd477e5a1f6b
Levente Molnar <molnr.levente@gmail.com>
2021-07-22 08:40:51 +0200

    add js for nagivagtion buttons

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,6 +1,16 @@
 const hamburger = document.querySelector('.hamburger');
 const navbar = document.querySelector('.navbar');
+const navButton = document.querySelectorAll('.nav-button');
 
 hamburger.addEventListener('click', () => {
     navbar.classList.toggle('change');
 });
+
+for (i in navButton) {
+    navButton[i].addEventListener('click', () => {
+        navbar.classList.toggle('change');  
+    });
+}
+// navButton.addEventListener('click', () => {
+//     navbar.classList.toggle('change');  
+// });
